3d6bad04f95c89710880b5e91c923d5ff7e74775
mickeyfrimpong <ofosufma@mail.uc.edu>
2022-03-06 10:57:15 -0500

    final commit

---
+++ src/CtoF.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class CtoF {
+
+    public static void main(String[] args) {
+        double cValue = 0;
+        double fValue;
+        String trash = " ";
+        boolean done = false ;
+
+        Scanner console = new Scanner(System.in);
+
+
+        System.out.println("Please input your Celsius value to convert to Fahrenheit ");
+        do {
+            if (console.hasNextDouble())
+            {
+                cValue = console.nextDouble();
+                console.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = console.nextLine();
+                System.out.println("Invalid input: " + trash + " re-run program");
+            }
+        } while(!done);
+
+        fValue = ((cValue * 1.8) + 32);
+
+        System.out.println("Your temperature in Fahrenheit is: " + fValue + "° F");
+
+        if (fValue >= 120)
+        {
+            System.out.println("Your temperature is at (or above) boiling point");
+        }
+        else if (fValue <= 32)
+        {
+            System.out.println("Your temperature is at (or below) freezing point");
+        }
+        else
+        {
+            System.out.println("Your temperature is neither freezing nor boiling");
+        }
+
+    }
+}
+
