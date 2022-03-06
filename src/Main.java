e52b73e473964cbe01d3f4eaa13260bf80308639
Mckelvin Ofosu-Frimpong <mickeyfrimpong@Mckelvins-MBP.home>
2022-03-06 10:48:28 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Part A
+        // 1.
+       // for(int x=0; x<=30; x++)
+        {
+           // System.out.printf("%4d",x);
+        }
+
+        // 2.
+        //for(int x=30; x>=1; x--)
+        {
+           // System.out.printf("%4d",x);
+        }
+
+        // 3.
+       // for(int x=0; x<=18; x+=3)
+        {
+            //System.out.printf("%4d",x);
+        }
+
+        // 4.
+        //for(int x=10; x>=0; x-=2)
+        {
+            //System.out.printf("%4d",x);
+        }
+
+
+        // Part B
+        //5.
+       // for(int row=1; row <=5; row++)
+        {
+           // for (int col = 1; col <= row; col++)
+            {
+             //   System.out.print("*");
+            }
+           // System.out.println();
+        }
+
+        //6.
+       // for(int row=1; row <=5; row++)
+        {
+           // for (int col = row; col <= 5; col++)
+            {
+              //  System.out.print("*");
+            }
+           // System.out.println();
+        }
+
+        // 7.
+         for(int row=1; row <=5; row++)
+        {
+            for (int col = 1; col <= 5; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
